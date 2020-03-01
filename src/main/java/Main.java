d2143569f85ed99f71ea5c5a412db91eeb33f64c
KrystianJanowicz <krystianjanowicz97@gmail.com>
2020-03-01 14:18:25 +0100

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Pesel pesel = new Pesel();
+        pesel.setPesel("44051401359");
+
+        System.out.println(pesel.getPesel());
+    }
+}
---
+++ src/main/java/Pesel.java
@@ -0,0 +1,69 @@
+
+
+public class Pesel {
+
+    String pesel;
+
+    public String getPesel() {
+        if(validatePesel(pesel)){
+            return pesel;
+        }
+        return "";
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    private boolean validatePesel(String pesel){
+        if(isLengthOk(pesel) && checkSum(pesel)){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isLengthOk(String pesel) {
+        if (pesel.length() != 11) {
+            System.out.println("Podany pesel nie ma 11 znaków");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    static int[] peselConvert(String s) {
+
+        int[] intArray = new int[s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            intArray[i] = Character.digit(s.charAt(i), 10);
+        }
+        return intArray;
+    }
+
+    private boolean checkSum(String pesel){
+        int PESEL[] = peselConvert(pesel);
+        int sum = 9 * PESEL[0] +
+                7 * PESEL[1] +
+                3 * PESEL[2] +
+                1 * PESEL[3] +
+                9 * PESEL[4] +
+                7 * PESEL[5] +
+                3 * PESEL[6] +
+                1 * PESEL[7] +
+                9 * PESEL[8] +
+                7 * PESEL[9];
+
+        sum %= 10;
+
+        if (sum == PESEL[10]) {
+            return true;
+        }
+        else {
+            System.out.println("Podany pesel nie jest prawidłowy (suma kontrolna)");
+            return false;
+        }
+    }
+
+
+}
